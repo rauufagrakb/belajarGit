28717c272c72c33acc6942d33dcb96da3fead6b2
rauuf <rauuf2100018069@webmail.uad.ac.id>
2023-07-30 19:16:18 +0700

    Gass

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.nav');
+async function logMovies() {
+  try {
+    const response = await fetch("data/siswa.json");
+    const movies = await response.json();
+    //console.log(movies);
+    return movies;
+  } catch (error) {
+    return new Error("Salaha goblok");
+  }
+  
+}
+const data = document.getElementsByTagName("a");
+logMovies().then((response)=>{
+  console.log(response[0].nama)
+}).catch((error)=>{
+  console.log('datane ora bener mas')
+})
+console.log("macan")
 
-menuToggle.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
+
